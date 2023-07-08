560c0c879b7911399a8380b47cad3a7ec183b52d
Peter Khalil <rikokamil97@gmail.com>
2023-07-08 22:44:34 +0300

    Change the title of page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { QueryClientProvider, QueryClient } from "react-query"
 import {ReactQueryDevtools} from "react-query/devtools"
 const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+      <title>Generate Advice</title>
+      </Head>
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
     </QueryClientProvider>
